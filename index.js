da7b7db3afa5858705ff9f6e486b2c81debf13f8
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-07-03 15:26:38 -0400

    Questions Posted

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+var questionsArr = [
+    {
+      question: 'C.F. Martin & Company (often referred to as Martin) is an American guitar manufacturer established in: ?',
+      answer: '1883 in Nazareth, Pennsylvania',
+      options: [
+        '1801 in Boston, Massachusetts ',
+        '1883 in Nazareth, Pennsylvania',
+        '1790 in Providence, Rhode Island',
+        '1850 in Las Vegas, Nevada'
+      ]
+    },
+    {
+        question: 'Snails and slugs, belong to a large taxonomic class of invertebrates within which phylum? ',
+        answer: 'Mollusca',
+        options: [
+          'Chordata',
+          'Nematoida ',
+          'Mollusca',
+          'Ambulacraria'
+        ]
+      },
+      {
+        question: 'How much does an Formula 1 car weigh in the 2022 season?',
+        answer: '1752lbs',
+        options: [
+          '1752lbs',
+          '1900lbs',
+          '3500lbs',
+          '1500lbs'
+        ]
+      },
+      {
+        question: 'What kind of airplane does Snoopy fly?',
+        answer: 'Sopwith Camel',
+        options: [
+          'Sopwith Camel',
+          'Zeppelin-Lindau D.I',
+          'Curtiss JN-4',
+          'Handley Page V/1500'
+        ]
+      },
+      {
+        question: 'What is a group of Pandas called?',
+        answer: 'An embarrassment',
+        options: [
+          'An option',
+          'A gaggle',
+          'An embarrassment',
+          'a tangle',
+          'a hoard'
+        ]
+      },
+  ]
